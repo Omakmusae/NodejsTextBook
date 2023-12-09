9ecccfa2fbfa706543bd03052e83a3acdfca4536
Omakmusae <pak1208926@gmail.com>
2023-12-09 23:10:48 +0900

    초기 세팅

---
+++ 1_JS/1_callstack.js
@@ -0,0 +1,17 @@
+function first() {
+    second();
+    console.log('첫번쨰')
+}
+
+
+function second() {
+    third();
+    console.log('두번쨰')
+}
+
+
+function third() {
+    console.log('세번쨰')
+}
+
+first();
