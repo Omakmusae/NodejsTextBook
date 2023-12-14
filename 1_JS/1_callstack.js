1f700ceba76d950d918d818f6f8784a203feea85
Omakmusae <pak1208926@gmail.com>
2023-12-15 08:32:28 +0900

    노드 기본 기능 익히기 : (1) REPL 과 js파일 실행 (2) CommmonJS 모듈 시스템

--- 1_JS/1_callstack.js
+++ 1_JS/1_callstack.js
@@ -16,9 +16,9 @@ function third() {
 
 first();
 
-//stack 구조 (크롬기준)
+//stack 구조 (크롬기준) : 아래부터 쌓이고 위에서부터 실행되며 제거됨
 //third()
 //second()
 //first()
-//anonymous
+//anonymous : 가상의 전역 컨텍스트 (항상 있다고 생각하자)
 //실행 결과 : 세번째, 두번째, 첫번째
---
+++ 2_node_basic/2_1_test.js
@@ -0,0 +1,4 @@
+const value = require('./2_CommonJS_module_system')
+
+console.log(value)
+//{odd: '홀수', even: '짝수'}
