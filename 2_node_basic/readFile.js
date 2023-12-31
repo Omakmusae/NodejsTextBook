ddbd1f1e6000f8676a224078d6778c7da0d4c87e
Omakmusae <pak1208926@gmail.com>
2023-12-31 18:00:52 +0900

    파일 시스템 예제 추가

---
+++ 2_node_basic/readFile.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises;
+fs.writeFile('./writeMe.txt', '글이 입력됩니다.2')
+    .then(() => {
+        return fs.readFile('./writeMe.txt')
+    })
+    .then((data)=> {
+        console.log(data.toString())
+    })
+    .catch((err) => {
+        throw err;
+    })
